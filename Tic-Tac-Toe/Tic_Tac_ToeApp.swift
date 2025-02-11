7d87649e0f78432f7d42feaf056a00289feabe5a
Daniel <73743252+dan-codes1@users.noreply.github.com>
2025-02-10 19:25:23 -0700

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Daniel Eze on 2025-02-10.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
